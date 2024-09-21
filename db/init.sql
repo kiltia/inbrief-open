ba4b09db3f8560a3f7d4ca61207de9bdb0deb61d
Nikita Rydanov <find.art.in.living@gmail.com>
2024-09-21 12:45:44 +0400

    feat: Added sources caching

--- db/init.sql
+++ db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE channel (
     about varchar(512)
 );
 
-CREATE TABLE folder ( 
+CREATE TABLE folder (
     chat_folder_link varchar(64) NOT NULL PRIMARY KEY,
     channels bigint[] NOT NULL
 );
@@ -19,12 +19,26 @@ CREATE TABLE source (
     source_id bigint NOT NULL,
     channel_id bigint NOT NULL,
     text text NOT NULL,
-    date varchar(19) NOT NULL,
+    timestamp timestamp with time zone NOT NULL,
     reference varchar(64) NOT NULL,
     embeddings jsonb NOT NULL,
     views bigint NOT NULL,
     label varchar(16),
     comments text[],
     reactions jsonb,
+
     PRIMARY KEY (channel_id, source_id)
+);
+
+CREATE TABLE request_source (
+    request_id uuid NOT NULL,
+    source_id uuid NOT NULL,
+
+    PRIMARY KEY (request_id, source_id)
+);
+
+CREATE TABLE processed_intervals (
+    l_bound timestamp with time zone NOT NULL,
+    r_bound timestamp with time zone NOT NULL,
+    request_id uuid NOT NULL PRIMARY KEY
 );
